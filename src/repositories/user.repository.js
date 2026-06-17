a650827df41703a5aa8d37f51d031960d2f79cf9
Mateus Saraiva <mateus.saraiva09@outlook.com>
2026-06-17 03:54:53 -0300

    feat(users): implement user registration flow

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,11 +1,27 @@
 const prisma = require("../lib/prisma");
 
+async function findByEmail(email) {
+    return prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+}
+
 async function create(data) {
     return prisma.user.create({
         data,
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            phone: true,
+            createdAt: true,
+        },
     });
 }
 
 module.exports = {
     create,
-};
+    findByEmail
+}
